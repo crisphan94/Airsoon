c37888f183381f98b1f8b565bdd40441df955c75
crisphan94 <pvtan103@gmail.com>
2025-03-28 16:12:36 +0700

    feat: add proxy humanity

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  server: { port: 3000 }
-})
+  server: {
+    port: 3000,
+    proxy: {
+      "/humanity": {
+        target: "https://rpc.testnet.humanity.org/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/humanity/, ""),
+      },
+    },
+  },
+});
